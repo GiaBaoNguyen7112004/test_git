2a8d5ff91dbbf96043f3e83bc85266f83a77f86b
BaoDev <giabao7112004@gmail.com>
2025-03-19 11:05:20 +0700

    first commit

---
+++ testjava.java
@@ -0,0 +1,15 @@
+public class testjava {
+    void A()
+    {
+        System.out.println("Method A action");
+    }
+
+    void B()
+    {
+        System.out.println("Method B action");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
