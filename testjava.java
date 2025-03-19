85325c446f3f7fd22c5754c6dcbfc09906aa6b73
BaoDev <giabao7112004@gmail.com>
2025-03-19 11:40:31 +0700

    thay doi b

--- testjava.java
+++ testjava.java
@@ -2,6 +2,7 @@ public class testjava {
     void B()
     {
         System.out.println("Method B action");
+        System.out.println("Method B action");
     }
 
     public static void main(String[] args) {
